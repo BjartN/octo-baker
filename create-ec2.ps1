712170340e46674661799d4188568804a407f73f
BjartN <bjartn@gmail.com>
2018-08-18 21:55:05 +0200

    Adapted to run in Octopus

--- create-ec2.ps1
+++ create-ec2.ps1
@@ -3,20 +3,49 @@
 #   2) Set-ExecutionPolicy RemoteSigned
 #   3) Install-Module -Name AwsPowerShell
 #   4) You can find your AWS credentials under the IAM User in the AWS Console
- 
-#. ./set-variables.ps1
-. ./set-variables.ps1
+$isOctopus = ![string]::IsNullOrEmpty($EC2_INSTANCENAME) 
 
-write-host "Variables are"
-write-host "`tInstance Name: $($instanceName)"
-write-host "`tAccessKey: $($accessKey)"
-write-host "`tSecret Key: $($secretKey)"
-write-host "`tSecurity Group: $($existingSecurityGroup)"  
-write-host "`tAMI: $($ami)"
-write-host "`tRegion: $($region)"
+$variablesFile = "./set-variables.ps1"
+$customVariablesFile = "./set-variables-custom.ps1"
+if ($isOctopus) {
+    $instanceName = $EC2_INSTANCENAME
+    $accessKey = $AWS_ACCESS_KEY
+    $secretKey = $AWS_SECRET_KEY
+    $ami = $AWS_AMI
+    $region = $AWS_REGION
+    $existingSecurityGroup = $AWS_EXISTING_SECURITY_GROUP
+    $octopusApiKey = $OCTOPUS_API_KEY
+    $octopusServerUrl = $OCTOPUS_SERVER_URL
+    $userDataRaw = $EC2_USER_DATA
 
+    $keyName = $instanceName
+    $keyFolder = "c:\keys\"
+    $fullKeyPath = "$($keyFolder)$($keyName).pem"
+    $awsProfile = "MyProfile"
+    $userDataFile = "$(Get-Location)\install-tentacle.ps1"
+}
+elseif (Test-Path $customVariablesFile) {
+    #ask for variables
+    . $customVariablesFile
+}
+elseif (Test-Path $variablesFile) {
+    #pre-defined variables
+    . $variablesFile
+}
+
+Write-Host "Variables are"
+Write-Host "`tInstance Name: $($instanceName)"
+Write-Host "`tAccessKey: $($accessKey)"
+Write-Host "`tSecret Key: $($secretKey)"
+Write-Host "`tSecurity Group: $($existingSecurityGroup)"  
+Write-Host "`tAMI: $($ami)"
+Write-Host "`tRegion: $($region)"
 Write-Host "Reading and encoding user data file from $($userDataFile)"
-$userDataRaw = Get-Content -Raw $userDataFile
+
+if (!$isOctopus) {
+    $userDataRaw = Get-Content -Raw $userDataFile
+}
+
 $userDataRaw = $userDataRaw -replace "{OCTOPUS_API_KEY}", $octopusApiKey
 $userDataRaw = $userDataRaw -replace "{OCTOPUS_SERVER_URL}", $octopusServerUrl
 $userDataRaw = $userDataRaw -replace "{TENTACLE_NAME}", $instanceName
@@ -30,8 +59,19 @@ $keyPair = New-EC2KeyPair `
     -Region $region `
     -ProfileName $awsProfile `
     -KeyName  $keyName 
-$keyPair.KeyMaterial | Out-File -Encoding ascii $fullKeyPath
-Write-Host "`t$($fullKeyPath)"
+
+if ($isOctopus) {
+    # Write-Host "<key>"
+    # Write-Host $keyPair.KeyMaterial
+    # Write-Host "</key>"
+    $keyPair.KeyMaterial | Out-File -Encoding ascii ./key.pem
+    New-OctopusArtifact -Path ./key.pem -Name "key.pem"
+    Write-Host "Key written as artifact"
+}
+else {
+    $keyPair.KeyMaterial | Out-File -Encoding ascii $fullKeyPath
+    Write-Host "`t$($fullKeyPath)"
+}
 
 Write-Host "Creating EC2 Instance"
 $instance = New-EC2Instance `
